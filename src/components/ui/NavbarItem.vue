03a7d19902b64f943d217d3b17cbe74cf1aad58c
LEE JINYOUNG <swlsdud100559@gmail.com>
2024-11-20 10:48:39 +0900

    메뉴바 수정

--- src/components/ui/NavbarItem.vue
+++ src/components/ui/NavbarItem.vue
@@ -1,12 +1,43 @@
 <script setup>
 import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
 
 // 변수
 const getUser = ref();
+const router = useRouter();
+const pathname = window.location.pathname;
+const beforeLoginMenu = [
+  {
+    title: "로그인",
+    url: "/login",
+  },
+];
+const afterLoginMenu = [
+  {
+    title: "장바구니",
+    url: "/cart",
+  },
+  {
+    title: "프로필",
+    url: "/profile",
+  },
+];
+const isToggle = ref(false);
 
-const logout = () => {
-  localStorage.clear();
-  window.location.reload();
+// 토글바 전환
+const sidebarToggle = () => {
+  isToggle.value = !isToggle.value;
+};
+
+// 로그아웃
+const logout = async () => {
+  try {
+    isToggle.value = false;
+    localStorage.clear();
+    router.replace("/");
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 onMounted(() => {
@@ -20,13 +51,14 @@ onMounted(() => {
     >
       <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <span
-          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white hover:text-red-500"
           >북 스토어</span
         >
       </a>
       <button
         data-collapse-toggle="navbar-default"
         type="button"
+        @click="sidebarToggle"
         class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
         aria-controls="navbar-default"
         aria-expanded="false"
@@ -48,18 +80,28 @@ onMounted(() => {
           />
         </svg>
       </button>
-      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <div
+        class="w-full md:block md:w-auto"
+        id="navbar-default"
+        :class="isToggle ? '' : 'hidden'"
+      >
         <ul
           v-if="getUser == null"
           class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
-          <li>
+          <li v-for="(item, key) in beforeLoginMenu" :key="key">
             <a
-              href="/login"
-              class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+              :href="item.url"
+              class="block py-2 px-3 rounded md:bg-transparent md:p-0 hover:text-blue-700 font-semibold text-xl"
+              :class="
+                pathname == item.url
+                  ? ' text-blue-500 md:text-blue-500 md:dark:text-blue-500'
+                  : 'md:text-black md:dark:text-black'
+              "
               aria-current="page"
-              >로그인</a
             >
+              {{ item.title }}
+            </a>
           </li>
         </ul>
 
@@ -67,35 +109,26 @@ onMounted(() => {
           v-else
           class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
-          <li>
+          <li v-for="(item, key) in afterLoginMenu" :key="key">
             <a
-              href="/cart"
-              class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+              :href="item.url"
+              class="block py-2 px-3 rounded md:bg-transparent md:p-0 hover:text-blue-700 font-semibold text-xl"
+              :class="
+                pathname == item.url
+                  ? ' text-blue-500 md:text-blue-500 md:dark:text-blue-500'
+                  : 'md:text-black md:dark:text-black'
+              "
               aria-current="page"
-              >장바구니</a
-            >
-          </li>
-          <li>
-            <a
-              href="/profile"
-              class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-              aria-current="page"
-              >프로필</a
+              >{{ item.title }}</a
             >
           </li>
           <li>
             <button
               @click="logout"
-              class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+              class="block py-2 px-3 md:text-black md:dark:text-white rounded md:bg-transparent md:p-0 hover:text-blue-700 font-semibold text-xl dark:text-white"
             >
               로그아웃
             </button>
-            <!-- <a
-              href="/login"
-              class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-              aria-current="page"
-              >로그아웃</a
-            > -->
           </li>
         </ul>
       </div>
