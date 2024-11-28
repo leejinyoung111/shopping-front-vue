ff0ec9af0c374a7e84b49540d3a27c738d507c2b
LEE JINYOUNG <swlsdud100559@gmail.com>
2024-11-28 17:36:02 +0900

    api url 변경

--- vite.config.js
+++ vite.config.js
@@ -14,9 +14,9 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/egov_test": {
-        target: "http://localhost:8080/egov_test/", // 프록시 요청을 보낼 대상 URL
-        rewrite: (path) => path.replace(/^\/egov_test/, ""), // 경로 재작성(Ex. '/api/test' => 'https://서버.com/test')
+      "/api": {
+        target: "http://localhost:8080/api/", // 프록시 요청을 보낼 대상 URL
+        rewrite: (path) => path.replace(/^\/api/, ""), // 경로 재작성(Ex. '/api/test' => 'https://서버.com/test')
         changeOrigin: true, // 원본 서버에 요청 시 호스트 헤더 변경
       },
     },
